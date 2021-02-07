7233ce5a2fd07f784c323af2881c9be5e4a84832
Giorno Giovanna <naufala47@gmail.com>
2021-02-07 18:44:10 +0700

    ujian ke 4

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,101 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import axios from 'axios'
 
-import axios from 'axios';
-
-const Blog = () => {
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        getApi()
-    }, [])
-
-    const getApi = () => {
-        axios.get('http://www.omdbapi.com/?s=avenger&apikey=e01f82b8')
-            .then(function (response) {
-                // handle success
-                setUsers(response.data.Search)
-                console.log(response.data.Search);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+export default class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            datas: [],
+            isLoading: true,
+            errors: null
+        };
     }
 
+    getAPI = () => {
+        axios.get(`https://dummyapi.io/data/api/user/0F8JIqi4zwvb77FGz6Wt/post?limit=10`, { headers: { 'app-id': '601d01d436ec40adb3b5faf3' } }, { delayed: true })
+            .then(response =>
+                response.data.data.map(
+                    itsdata => ({
+                        image: `${itsdata.image}`,
+                        text: `${itsdata.text}`
+                    })
+                )
+            )
 
-    return (
-        <div>
-            {
-                users.map(post => {
-                    return (
-                        // < !--SECTION: BLOG-->
-                        <div className="section-vcardbody section-page" id="page-blog">
-                            <div className="section-blog">
+            .then(datas => {
+                this.setState(
+                    {
+                        datas,
+                        isLoading: false
+                    },
+                    console.log(datas)
+                );
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
 
-                                {/* <!-- Section title --> */}
-                                <h2 className="section-title">Blog</h2>
-                                {/* <!-- /Section title -->     */}
+    componentDidMount() {
+        this.getAPI();
+        console.log(this.getAPI());
 
-                                {/* <!-- BLOG POSTS --> */}
-                                <div className="blog-posts">
-                                    {/* <!-- blog-item --> */}
-                                    <div className="blog-item">
-                                        <div className="blog-item-wrapper">
-                                            {/* <!-- blog item thumbnail --> */}
-                                            <div class="blog-item-thumb">
-                                                <a href="single.html" class="loadPost"><img src={post.Poster} alt="" /></a>
-                                            </div>
-                                            {/* <!-- /blog item thumbnail --> */}
-                                            {/* <!-- Blog item - infos --> */}
-                                            <div class="blog-item-infos">
-                                                {/* <!-- blog-item-title --> */}
-                                                <div class="blog-item-title-wrapper">
-                                                    <h2 class="blog-item-title title-border"><a href="single.html" class="loadPost">{post.Title}</a></h2>
-                                                </div>
-                                                {/* <!-- /blog-item-title --> */}
-                                                {/* <!-- blog item - description --> */}
-                                                <div class="blog-item-description">
-                                                    <p><a href="single.html" class="loadPost">{post.Year}</a></p>
-                                                </div>
-                                                {/* <!-- /blog-item-description --> */}
-                                                {/* <!-- blog item - link --> */}
-                                                <div class="blog-item-link">
-                                                    <a href="single.html" class="btn btn-default loadPost">See More</a>
-                                                </div>
-                                                {/* <!-- /blog item - link --> */}
-                                            </div>
-                                            {/* <!-- /blog item - infos --> */}
-                                        </div>
-                                    </div>
-                                    {/* <!-- /blog-item --> */}
+    }
 
-                                    {/* <!-- Pagination --> */}
-                                    <div class="section-pagination">
-                                        <div class="row">
-                                            <div class="col-xs-12">
-                                                <span class="pagination-numbers">
-                                                    <a href="#">1</a>
-                                                    <a href="#">2</a>
-                                                    <a href="#">3</a>
-                                                    <a href="#">4</a>
-                                                    <a href="#">5</a>
-                                                    <a href="#">6</a>
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    {/* <!-- /Pagination --> */}
+    render() {
+        const { isLoading, datas } = this.state;
+        return (
+            <div className="section-vcardbody" id="page-blog">
+                <div className="section-blog">
+                    <h2 className="section-title">Blog</h2>
+                </div>
+
+                <div className="blog-posts">
+                    {this.state.datas.map((blog) => {
+                        return <div className="blog-item">
+                            <div className="blog-item-wrapper">
+                                <div className="blog-item-thumb">
+                                    <img src={blog.image} />
+                                    {blog.text}
                                 </div>
                             </div>
                         </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+                    })}
+                </div>
 
-export default Blog
+                <div className="section-pagination">
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <span className="pagination-numbers">
+                                <a href="#">1</a>
+                                <a href="#">2</a>
+                                <a href="#">3</a>
+                                <a href="#">4</a>
+                                <a href="#">5</a>
+                                <a href="#">6</a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        )
+    }
+}
